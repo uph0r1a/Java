9867794f87b7d70f0c1f9ce02751ef166974f2ec
uph0r1a <tdtri1712@gmail.com>
2026-06-04 13:45:15 +0700

    a

--- Day 1/src/Main.java
+++ Day 1/src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter your height: ");
+        float height = scanner.nextFloat();
+        scanner.nextLine();
+
+        System.out.print("Enter your favourite subject: ");
+        String subject = scanner.nextLine();
+
+        System.out.println("Hello! My name is " + name + ".\nI am " + age + " years old and " + height
+                + " cm tall. \nMy favorite subject is " + subject + ".");
     }
 }
