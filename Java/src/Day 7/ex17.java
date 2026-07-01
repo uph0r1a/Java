0115ddb1e21f0a004dd266f32c71137340ef9552
uph0r1a <tdtri1712@gmail.com>
2026-07-01 21:19:54 +0700

    a

--- Java/src/Day 7/ex17.java
+++ Java/src/Day 7/ex17.java
@@ -1,5 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ex17 {
+    static class PhoneBookEntry {
+        private String name, phoneNumber;
+
+        public PhoneBookEntry(String name, String phoneNumber) {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+    }
+
     public static void main(String[] args) {
-        
+        List<PhoneBookEntry> phoneBookEntries = new ArrayList<>(List.of(
+                new PhoneBookEntry("A", "0"),
+                new PhoneBookEntry("B", "1"),
+                new PhoneBookEntry("C", "2"),
+                new PhoneBookEntry("D", "3"),
+                new PhoneBookEntry("E", "4")));
+
+        for (PhoneBookEntry phoneBookEntry : phoneBookEntries) {
+            System.out.println(
+                    "Name: " + phoneBookEntry.getName() + "\nPhone Number: " + phoneBookEntry.getPhoneNumber());
+        }
     }
 }
